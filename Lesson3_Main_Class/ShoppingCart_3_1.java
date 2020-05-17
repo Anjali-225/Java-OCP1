2fa864042e5b0f6354b3c9f06279aaed52e75999
Anjali <anjali.payal69@gmail.com>
2020-05-17 13:35:34 +0200

    Lesson3_Main_Class

---
+++ Lesson3_Main_Class/ShoppingCart_3_1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson3_Main_Class;
+
+/**
+ *
+ * @author anjali
+ */
+public class ShoppingCart_3_1 {
+    //Exercise 3-1: Creating a Class
+    
+}
---
+++ Lesson3_Main_Class/ShoppingCart_3_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson3_Main_Class;
+
+/**
+ *
+ * @author anjali
+ */
+public class ShoppingCart_3_2 {
+    //Exercise 3-2: Creating a MAIN method
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Shopping Cart!");
+    } 
+}
