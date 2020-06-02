fcd267ef70046adce05628b1d2f25df9b5f4ae6f
Anjali <anjali.payal69@gmail.com>
2020-06-02 20:37:16 +0200

    Using_Interface

---
+++ Lesson13_Using_Interfaces/ShoppingCart_13_2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lesson13_Using_Interfaces;
+
+/**
+ *
+ * @author anjali
+ */
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+public class ShoppingCart_13_2 {
+    ArrayList<Item_13_2> items = new ArrayList();
+    
+    public static void main(String[] args){
+        ShoppingCart_13_2 cart = new ShoppingCart_13_2();
+        cart.fillCart();
+        cart.removeItemFromCart("Trousers");
+    }
+    
+    public void fillCart(){
+        items.add(new Shirt_13_2(40.95,'M','R'));
+        items.add(new Shirt_13_2(32.99,'M','Y'));
+        items.add(new Trousers_13_2(59.99,34,'B',"Relaxed",'M'));
+        items.add(new Trousers_13_2(75.50,8,'G',"Skinny",'F'));
+    }
+    
+    public void removeItemFromCart(String desc){
+        // remove all Trousers from the items list, then print out the list
+        items.removeIf(i -> i.getDesc().equals(desc));
+        System.out.println("Cart after removing Trousers: \n"+items);
+    }
+    
+}
